7862efe83e373597b759f7d40e14a37297ac0616
Chuanqi Sun <chuanqisun-no-reply@gmail.com>
2025-10-04 13:19:23 -0400

    more seamless generation

--- src/features/crowd/crowd.component.ts
+++ src/features/crowd/crowd.component.ts
@@ -29,7 +29,6 @@ export const CrowdComponent = createComponent(() => {
 
   keydownInterrupt$.subscribe(() => {
     speechQueue.clear();
-    audioPlayer.clearQueued();
   });
 
   escapeKeydown$.subscribe(() => {
